93cdbd5527a0c8e137601a3a16cd4d4f82f03e2f
lrc <3115679714@qq.com>
2020-04-04 11:09:40 +0800

    [UPD] 修改接口

--- springboot-test/src/main/java/com/example/springboottest/api/v1/controller/HelloController.java
+++ springboot-test/src/main/java/com/example/springboottest/api/v1/controller/HelloController.java
@@ -2,12 +2,14 @@ package com.example.springboottest.api.v1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ruicai.li@hand-china.com
  */
-@RestController("/v1/{orginazation_id}/hello")
+@RestController("vi.HelloController")
+@RequestMapping("/v1/{orginazation_id}/hello")
 public class HelloController {
 
     @GetMapping
