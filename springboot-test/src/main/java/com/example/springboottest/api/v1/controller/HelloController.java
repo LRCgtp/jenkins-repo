d1bceacc777c2af7da6360e4b280905f32480b60
lrc <3115679714@qq.com>
2020-04-04 11:01:23 +0800

    [ADD] 新增接口

--- springboot-test/src/main/java/com/example/springboottest/api/v1/controller/HelloController.java
+++ springboot-test/src/main/java/com/example/springboottest/api/v1/controller/HelloController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author ruicai.li@hand-china.com
  */
-@RestController("/v1/orginazation_id/hello")
+@RestController("/v1/{orginazation_id}/hello")
 public class HelloController {
 
-    @GetMapping("/{orginazation_id}")
+    @GetMapping
     public String getStr(@PathVariable(value = "orginazation_id")Long orginazationId){
         return "我是组织ID为+"+orginazationId+"的用户";
     }
